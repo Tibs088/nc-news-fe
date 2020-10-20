acb4849e190c72a437d3045d8eee1099b64f256b
Tibs088 <tibs088@gmail.com>
2020-10-20 12:54:07 +0100

    different images for each topic

--- src/components/Topics_Card_Comp.jsx
+++ src/components/Topics_Card_Comp.jsx
@@ -15,6 +15,12 @@ const useStyles = makeStyles({
   },
 });
 
+const imgLinks = {
+  coding: "https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
+  football: "https://images.theconversation.com/files/341806/original/file-20200615-65942-1hl134r.jpg?ixlib=rb-1.1.0&rect=161%2C41%2C3832%2C2616&q=45&auto=format&w=926&fit=clip",
+  cooking: "https://media3.s-nbcnews.com/j/newscms/2019_41/3044956/191009-cooking-vegetables-al-1422_ae181a762406ae9dce02dd0d5453d1ba.nbcnews-fp-1200-630.jpg"
+}
+
 export default function TopicsCard(props) {
   const classes = useStyles();
 
@@ -23,11 +29,12 @@ export default function TopicsCard(props) {
       <CardActionArea>
         <CardMedia
           component="img"
-          image="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+          image={imgLinks[props.topics.slug]}
           title="Contemplative Reptile"
         />
         <CardContent>
           <h3>{props.topics.slug}</h3>
+          <p>{props.topics.description}</p>
         </CardContent>
       </CardActionArea>
       <CardActions>
