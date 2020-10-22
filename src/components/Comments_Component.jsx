2a83309ad439b5a2ed5eda851818651a8f4d9819
Tibs088 <tibs088@gmail.com>
2020-10-22 14:55:00 +0100

    put the comments in cards with extra information, time to change the way voting works slightly

--- src/components/Comments_Component.jsx
+++ src/components/Comments_Component.jsx
@@ -1,19 +1,66 @@
+//React Stuff
 import React from 'react';
+import { Link } from '@reach/router';
+//Material UI For the Cards
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import Avatar from '@material-ui/core/Avatar';
+import { red } from '@material-ui/core/colors';
+import { Votes } from './Votes_Comp';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+}));
 
 export const Comments = (props) => {
+  const classes = useStyles();
 
   return (
     <div>
-      <ul>
-        {props.comments.map(comment => {
-          return <li key={comment.comment_id}>
-            <h4>{comment.author}</h4>
-            <p>{comment.body}</p>
-            <p>{comment.votes}</p>
-            <p>{comment.created_at}</p>
-          </li>
-        })}
-      </ul>
+      {props.comments.map(comment => {
+        return (
+          <Card key={comment.comment_id} className={classes.root} id='comment'>
+            <CardHeader
+              avatar={
+                <Avatar aria-label="user" className={classes.avatar} />}
+              title={comment.author}
+              subheader={comment.created_at}
+            />
+            <CardContent>
+              <p>{comment.body}</p>
+              <Votes></Votes>
+            </CardContent>
+          </Card>
+        )
+        // <li key={comment.comment_id}>
+        //   <h4>{comment.author}</h4>
+        //   <p>{comment.body}</p>
+        //   <p>{comment.votes}</p>
+        //   <p>{comment.created_at}</p>
+        // </li>
+      })}
     </div>
   )
 }
