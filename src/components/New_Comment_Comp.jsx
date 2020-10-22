bb163de78807db492d92e7877977e907195a6efc
Tibs088 <tibs088@gmail.com>
2020-10-22 12:31:08 +0100

    pre-lecture back up, have been working on posting comments using this material-ui text field

--- src/components/New_Comment_Comp.jsx
+++ src/components/New_Comment_Comp.jsx
@@ -6,7 +6,7 @@ const useStyles = makeStyles((theme) => ({
   root: {
     '& .MuiTextField-root': {
       margin: theme.spacing(1),
-      width: '25ch',
+      width: '500px',
     },
   },
 }));
@@ -14,17 +14,22 @@ const useStyles = makeStyles((theme) => ({
 export const CommentField = () => {
   const classes = useStyles();
 
+  const postComment = (comment) => {
+
+  }
+
   return (
     <form className={classes.root} noValidate autoComplete='off'>
       <div>
         <TextField
           id="outlined-multiline-static"
-          label="Multiline"
+          label="Comment"
           multiline
-          rows={4}
+          rows={10}
           defaultValue="Default Value"
           variant="outlined"
         />
+        <button onClick={() => postComment()}>Post</button>
       </div>
     </form>
   )
